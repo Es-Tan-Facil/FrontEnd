89cdeb9c3a3abc3ca14bc794a2809ee968ce56d2
Raul Villar <raultheviking@gmail.com>
2023-05-05 15:12:51 +0200

    90% done cards

--- src/Components/DonationCards/DonationCards.jsx
+++ src/Components/DonationCards/DonationCards.jsx
@@ -8,32 +8,32 @@ import { Card, CardHeader, CardBody, Typography } from "@material-tailwind/react
 export default function DonationCards() {
     return (
         <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginTop: "5px", gap: "20px" }}>
-            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black" >
-                <CardBody>
+            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black" style={{height: "253px", width:"213px"}}>
+                <CardBody style={{height:"7.5vh"}}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Compra KM
                     </Typography>
                 </CardBody>
-                <img src={Bus} alt="Bus" style={{width: "111px", height: "111px", margin: "auto"}} />
+                <img src={Bus} alt="Bus" style={{width: "111px", height: "111px", margin: "auto" }} />
             </Card>
-            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black">
-                <CardBody>
+            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black" style={{height: "253px", width:"213px"}}>
+                <CardBody style={{height:"7.5vh"}}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Apadrina una escuela
                     </Typography>
                 </CardBody>
                 <img src={SchoolBuilding} alt="School" style={{width: "137px", height: "126px", margin: "auto"}} />
             </Card>
-            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black">
-                <CardBody>
+            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black" style={{height: "253px", width:"213px"}}>
+                <CardBody style={{height:"7.5vh"}}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Material escolar
                     </Typography>
                 </CardBody>
                 <img src={SchoolMaterial} alt="School Material" style={{width: "78px", height: "125px", margin: "auto"}} />
             </Card>
-            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black">
-                <CardBody>
+            <Card className="mt-6 w-64 rounded-tr-lg rounded-bl-lg border-solid border border-black" style={{height: "253px", width:"213px"}}>
+                <CardBody style={{height:"7.5vh"}}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Más ayuda
                     </Typography>
