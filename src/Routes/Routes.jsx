bd38bc8b3996c7823f4a6102503f844b645218a0
Carlos Sánchez <holapueblodev@gmail.com>
2023-05-05 10:22:50 +0200

    Inicializando NavBar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "../Pages/HomePage/HomePage"
+import HomePage from '../Views/HomePage/HomePage';
 
 
 function Routes() {
 
     const router = createBrowserRouter([{
         path: "/",
-        element: <HomePage />
+        element: <HomePage/>
     }
     ])
 
