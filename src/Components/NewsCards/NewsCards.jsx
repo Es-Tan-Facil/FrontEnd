f2eb76da16f99c871e56690b33fc0cb202953fa4
celiacueria <celiacueria1999@gmail.com>
2023-05-08 12:10:27 +0200

    Bus route

--- src/Components/NewsCards/NewsCards.jsx
+++ src/Components/NewsCards/NewsCards.jsx
@@ -4,14 +4,29 @@ import 'tailwindcss/tailwind.css';
 
 
 export default function Example() {
+
+    let data = [{
+        title: "Viaje 2023",
+        subtitle: "A mar de plata",
+        img: "../../Assets/Pictures/carretera.jpg"
+    }]
+    
+console.info(data[0].title)
   return (
+    <>
+     {data.map((item) => 
     <div id="CardBorder" class="max-w-sm rounded overflow-hidden  border border-solid rounded-tl-none rounded-br-none rounded-tr-3xl rounded-bl-3xl pb-10 ">
       <img class="w-full rounded-br-none rounded-bl-3xl" src={carretera} alt="Sunset in the mountains" />
-      <div class="font-bold text-xl mb-2 ml-3">Viaje 2023</div>
+      <div class="font-bold text-xl mb-2 ml-3">{item.title}</div>
       <p class="text-gray-700 text-base pb-4 ml-4">
-        A Mar de Plata
+       {item.subtitle}
       </p>
      
     </div>
+                )}
+      
+      
+ 
+    </>
   );
 }
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
 import NewsCards from '../../Components/NewsCards/NewsCards'
 import './HomePage.css'
+import BusAnimation from '../../Components/BusAnimation/BusAnimation'
 function HomePage() {
   return (
     <>
     <NavBar/>
     <NewsCards />
+    <BusAnimation />
     </>
   )
 }
