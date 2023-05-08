db12c93c849135b75f4343b231f7ef910f86066c
Raul Villar <raultheviking@gmail.com>
2023-05-08 08:44:22 +0200

    finished cards

--- src/Components/DonationCards/DonationCards.jsx
+++ src/Components/DonationCards/DonationCards.jsx
@@ -8,38 +8,38 @@ import { Card, CardBody, Typography } from "@material-tailwind/react";
 
 export default function DonationCards() {
     return (
-        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginTop: "5px", gap: "20px", fontFamily: "Poppins"}}>
-            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{height: "253px", width:"213px"}}>
-                <CardBody style={{height:"7.5vh"}}>
+        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginTop: "5px", gap: "20px", fontFamily: "Poppins" }}>
+            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{ height: "253px", width: "213px" }}>
+                <CardBody style={{ height: "7.5vh" }}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Compra KM
                     </Typography>
                 </CardBody>
-                <img src={Bus} alt="Bus" style={{width: "111px", height: "111px", margin: "auto" }} />
+                <img src={Bus} alt="Bus" style={{ width: "111px", height: "111px", margin: "auto" }} />
             </Card>
-            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{height: "253px", width:"213px"}}>
-                <CardBody style={{height:"7.5vh"}}>
+            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{ height: "253px", width: "213px" }}>
+                <CardBody style={{ height: "7.5vh" }}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Apadrina una escuela
                     </Typography>
                 </CardBody>
-                <img src={SchoolBuilding} alt="School" style={{width: "137px", height: "126px", margin: "auto"}} />
+                <img src={SchoolBuilding} alt="School" style={{ width: "137px", height: "126px", margin: "auto" }} />
             </Card>
-            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{height: "253px", width:"213px"}}>
-                <CardBody style={{height:"7.5vh"}}>
+            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{ height: "253px", width: "213px" }}>
+                <CardBody style={{ height: "7.5vh" }}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Material escolar
                     </Typography>
                 </CardBody>
-                <img src={SchoolMaterial} alt="School Material" style={{width: "78px", height: "125px", margin: "auto"}} />
+                <img src={SchoolMaterial} alt="School Material" style={{ width: "78px", height: "125px", margin: "auto" }} />
             </Card>
-            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{height: "253px", width:"213px"}}>
-                <CardBody style={{height:"7.5vh"}}>
+            <Card className="mt-6 w-64 rounded-none rounded-tr-3xl rounded-bl-3xl border-solid border border-black" style={{ height: "253px", width: "213px" }}>
+                <CardBody style={{ height: "7.5vh" }}>
                     <Typography variant="h5" color="blue-gray" className="mb-2" style={{ textAlign: "center" }}>
                         Más ayuda
                     </Typography>
                 </CardBody>
-                <img src={Help} alt="Help Us" style={{width: "134px", height: "111px", margin: "auto"}} />
+                <img src={Help} alt="Help Us" style={{ width: "134px", height: "111px", margin: "auto" }} />
             </Card>
         </div>
     )
