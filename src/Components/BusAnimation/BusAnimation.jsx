62c4fd8596379577f351a5dd3c8b3db323fea19e
celiacueria <celiacueria1999@gmail.com>
2023-05-09 11:24:15 +0200

    Bus position

--- src/Components/BusAnimation/BusAnimation.jsx
+++ src/Components/BusAnimation/BusAnimation.jsx
@@ -4,16 +4,16 @@ import bus from '../../Assets/Pictures/bus.svg'
 import './BusAnimation.css'
 import { useState } from 'react'
 const BusAnimation = () => {
-
     const[posX, setPosX] = useState();
-    
+        const style = { '--posX': `${posX}vw` };
+
   return (
     <>
     <div className='BusAnimation'>
     
       <img src={background} alt="" className='BackgroundImg' />
       <div className="sub_div">
-        <img src={bus} alt="" className='BusImg' style={{ transform: `translate(${posX}vw)` }} />
+      <img src={bus} alt="" className='BusImg' style={style} />
       </div>
     </div>
     </>
