3ff19eefd530c2899a3f273f717768ea2fc3d58e
Raul Villar <raultheviking@gmail.com>
2023-05-11 11:57:00 +0200

    Fixed text responsiveness

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Text({ text }) {
     return (
         <>
-            <p style={{ fontSize: "20px", color: "rgba(0, 0, 0, 1)" }}>{text}</p>
+            <p style={{ fontSize: "1.2rem", color: "rgba(0, 0, 0, 1)" }}>{text}</p>
         </>
     )
 }
