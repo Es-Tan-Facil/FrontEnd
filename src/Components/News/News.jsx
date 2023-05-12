aa6c5b28e0a9b1bd87b58db7d712184ae6dabd81
Raul Villar <raultheviking@gmail.com>
2023-05-12 12:04:03 +0200

    Added News Page

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import NewsCards from '../NewsCards/NewsCards.jsx'
-import carretera from '../../Assets/Pictures/carretera.jpg';
+import React, { useState, useEffect } from 'react';
+import HTTPService from '../../Services/HTTPService.jsx';
+import Title from '../Title/Title.jsx';
+import NewPrototype from '../NewPrototype/NewProtype.jsx';
 
 function News() {
-    let data = [{
-        title: "Viaje 2023",
-        subtitle: "A mar de plata",
-        img: `${carretera}`
-    }]
-  return (
-    <div title="UltimasNoticias" id='UltimasNoticias'>
-    {data.map((item) => 
+  const [cards, setCards] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPage = 2;
 
-      <NewsCards item={item}/>
-    
-      )}
+  useEffect(() => {
+    async function fetchNews() {
+      try {
+        const response = await HTTPService().getAllData();
+        console.info(response);
+        setCards(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchNews();
+  }, []);
+
+  const startIndex = (currentPage - 1) * perPage;
+
+  return (
+    <div className="flex flex-col items-center mt-12">
+      <Title title="Últimas Noticias" />
+      <div className="w-4/5 mx-auto md:w-2/5 mx-auto">
+        {cards.slice(startIndex, startIndex + perPage).map((item) => (
+          <div key={item.id} className="mb-4 mt-10">
+            <NewPrototype card={item} />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="px-2 py-1 rounded-md bg-blue-500 text-white mr-2"
+        >
+          Anterior
+        </button>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={startIndex + perPage >= cards.length}
+          className="px-2 py-1 rounded-md bg-blue-500 text-white"
+        >
+          Siguiente
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default News
+export default News;
+
+
