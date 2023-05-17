b589c82da91ae4793768cb23d4223f55ea597e4c
lucia <luciatiradoturiel@gmail.com>
2023-05-17 12:45:05 +0200

    updates adminpage

--- src/Components/NewsAdmin/NewsAdmin.jsx
+++ src/Components/NewsAdmin/NewsAdmin.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import HTTPService from '../../Services/HTTPService.jsx';
 import Title from '../Title/Title.jsx';
+import InputPhoto from '../InputPhoto/InputPhoto.jsx';
 
 
 
 
 function NewsAdmin() {
     const [cards, setCards] = useState([]);
+    
 
     useEffect(() => {
         async function fetchNews() {
@@ -21,51 +23,41 @@ function NewsAdmin() {
         fetchNews();
     }, [HTTPService]);
 
+    
 
-    async function handleEdit(cards) {
-        try {
-          const updatedNews = { ...cards }; 
-          const response = await HTTPService().updateData(cards.id, updatedNews); 
-          console.log(response); 
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      
-    const handleDelete = async (cardId) => {
-        try {
-            const response = await HTTPService().deleteData(cardId);
-            setCards(cards.filter((card) => card.id !== cardId));
-            console.log(response); 
-        } catch (error) {
-            console.log(error);
-        }
-    };
+
+        const handleDelete = async (cardId) => {
+            try {
+                const response = await HTTPService().deleteData(cardId);
+                setCards(cards.filter((card) => card.id !== cardId));
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
 
 
+        return (
 
 
-    return (
-
-
-        <div id="NewsAdmin" className="md:h-[100vh] flex flex-col items-center">
-            <Title title="Últimas Noticias" />
-            <div className="flex flex-wrap gap-12">
-                {cards.reverse().map((card) => (
-                    <div key={card.id}>
-                        <img src={card.urlImg} alt={card.title} className="w-48 h-48 object-cover" />
-                        <h2>{card.title}</h2>
-                        <p>{card.description}</p>
-                        <small>{card.date}</small>
-                        <button onClick={() => handleEdit(card.id)} className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Editar</button>
-                        <button onClick={() => handleDelete(card.id)} className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded">Borrar</button>
-                    </div>
-                ))}
+            <div id="NewsAdmin" className="md:h-[100vh] flex flex-col items-center">
+                <Title title="Últimas Noticias" />
+                <div className="flex flex-wrap gap-12">
+                    {cards.reverse().map((card) => (
+                        <div key={card.id}>
+                            <img src={card.urlImg} alt={card.title} className="w-48 h-48 object-cover" />
+                            <h2>{card.title}</h2>
+                            <p>{card.description}</p>
+                            <small>{card.date}</small>
+                            <button className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Editar</button>
+                            <button onClick={() => handleDelete(card.id)} className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded">Borrar</button>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
 
-export default NewsAdmin;
+    export default NewsAdmin;
