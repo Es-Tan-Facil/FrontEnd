8f58d1ac4f5ab110a66f08ee59834062d12fcd45
Raul Villar <raultheviking@gmail.com>
2023-05-19 12:25:15 +0200

    Deleted the background from news pages

--- src/Pages/NewsPage/NewsPage.jsx
+++ src/Pages/NewsPage/NewsPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import background from '../../Assets/Pictures/BackgroundAmerica.svg'
 import ButtonBackToTop from '../../Components/ButtonBackToTop/ButtonBackToTop'
 import News from '../../Components/News/News'
 import HeaderPages from '../../Components/HeaderPages/HeaderPages'
@@ -7,9 +6,6 @@ import HeaderPages from '../../Components/HeaderPages/HeaderPages'
 export default function NewsPage() {
     return (
         <>
-            <div className="containerHome">
-                <img data-testid="background-img" src={background} alt="imagen" />
-            </div>
             <div data-testid="header-component">
                 <HeaderPages stateImg={true}/>
             </div>
