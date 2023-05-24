1fec6d33d98e9ceb1ce23530e88cdc91d8586c29
lucia <luciatiradoturiel@gmail.com>
2023-05-24 14:19:56 +0200

    travel text

--- src/Components/OurTrip/OurTrip.jsx
+++ src/Components/OurTrip/OurTrip.jsx
@@ -8,6 +8,19 @@ import background from '../../Assets/Pictures/mapa_back.svg'
 
 
 export default function OurTrip() {
+    const description = <React.Fragment><br/>¡Bienvenidos a nuestra aventura!<br/>
+
+    Aquí queremos compartir con ustedes la inspiradora historia de nuestro viaje anual, un viaje lleno de sueños, solidaridad y amor por los demás. Cada año, nos embarcamos en una aventura desde San Luis hasta Mar de Plata, acompañados de los maravillosos niños de las escuelas rurales de la zona de Bajo de Veliz.
+    
+    Este viaje no sería posible sin las donaciones desinteresadas de voluntarios y personas que desean colaborar.<br/> Gracias a su generosidad, logramos que estos increíbles chicos puedan conocer y disfrutar del mar. Para muchos de ellos, el mundo se limita a su región, pero esta travesía les permite abrir sus ojos a otras realidades y vivir experiencias inolvidables.
+    
+    Nuestro recorrido comienza en un autobús, atravesando más de 1175 km de distancia. En cada parada, sentimos que nuestro sueño está un paso más cerca de convertirse en realidad. Es emocionante ver cómo mucha gente ya conoce nuestra ruta y nos ofrece su apoyo. En estos momentos, nos acercan víveres para el resto del viaje, refrescos y todo tipo de gestos amables. Incluso los negocios locales de Mar de Plata, como restaurantes y hostels, colaboran con nosotros al brindarnos cenas y alojamiento para los chicos.<br/> Su generosidad nos llena de gratitud y nos motiva a seguir adelante.<br/>
+    
+    Nuestro sueño es que esta hermosa travesía nunca termine. Anhelamos continuar cumpliendo los sueños de estos niños año tras año. Por eso, queremos invitarte a que te sumes a esta maravillosa causa.<br/> 
+    
+    Juntos, podemos hacer posible que más chicos conozcan el mar, exploren nuevos horizontes y descubran un mundo lleno de posibilidades. Únete a nuestra familia de soñadores y hagamos que cada año sea una experiencia inolvidable.
+    
+    ¡Gracias por visitar nuestra web y por considerar ser parte de este viaje extraordinario!</React.Fragment>
     return (
         <div id="OurTrip" className=" md:justify-start flex justify-center items-center flex-row flex-wrap gap-20 md:py-4 md:h-[100vh] mt-12 md:ml-40">
             <div className="containerHome hidden md:block">
@@ -19,10 +32,7 @@ export default function OurTrip() {
             <div id="textTrip" className=" text-justify md:flex-row md:justify-start md:items-start">
                 <Title title="Nuestro viaje"/>
                 <StainTitle />
-                <Text text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. But also the leap into electronic typesetting, remaining essentially unchanged. 
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
-                versions of Lorem Ipsum." />
+                <Text text={description} />
             </div>
 
         </div>
